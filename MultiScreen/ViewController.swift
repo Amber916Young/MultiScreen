3db87e2e60240fd230f67a6607edf1aa70e24dd6
Amber916Young <youngyejia@163.com>
2022-03-05 13:05:21 +0000

    Initial Commit

---
+++ MultiScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiScreen
+//
+//  Created by birdy young on 05/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
